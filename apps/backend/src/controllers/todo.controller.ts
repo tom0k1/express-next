bb3c70d4bacc769b3d83d1659c17292d0da4780b
Tomoki Yokoyama <tom0k1@Tomokis-MacBook-Air.local>
2024-12-29 01:41:42 +0900

    init

--- apps/backend/src/controllers/todo.controller.ts
+++ apps/backend/src/controllers/todo.controller.ts
@@ -1,9 +1,10 @@
 import { Task } from "../../db/models/task.model";
+import { Response, Request } from "express";
 
 export default class TodoController {
-  async list() {
+  async list(req: Request, res: Response) {
     const tasks = await Task.query();
 
-    return tasks;
+    res.json(tasks);
   }
 }
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,17 +1,43 @@
-import { CounterButton } from "@repo/ui/counter-button";
+"use client";
 
-export const metadata = {
-  title: "Todo App",
-};
+import Skeleton from "@repo/ui/src/skeleton";
+import { useEffect, useState } from "react";
+import { Calendar } from "@nextui-org/react";
+import { parseDate } from "@internationalized/date";
 
 export default function TopPage(): JSX.Element {
+  const [tasks, setTasks] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL!}/todo/list`
+        );
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error("Fetching data failed", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <h1 className="title">
         TODO <br />
         <span>アプリ</span>
       </h1>
-      <CounterButton />
+      <Skeleton />
+      <div className="flex gap-x-4">
+        <Calendar aria-label="Date (No Selection)" />
+        <Calendar
+          aria-label="Date (Uncontrolled)"
+          defaultValue={parseDate("2020-02-03")}
+        />
+      </div>
     </div>
   );
 }
