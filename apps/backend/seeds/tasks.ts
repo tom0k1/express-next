8bf6025962a1d6f199a3fcb847ea1ded19f8d0be
Tomoki Yokoyama <tom0k1@Tomokis-MacBook-Air.local>
2024-12-29 02:16:58 +0900

    init

--- apps/backend/seeds/tasks.ts
+++ apps/backend/seeds/tasks.ts
@@ -6,4 +6,9 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Inserts seed entries
   await knex("tasks").insert([{ id: 1, user_id: 1, name: "ジムに行く" }]);
+  await knex("tasks").insert([
+    { id: 2, user_id: 1, name: "ジムに行く2", due_date: new Date() },
+  ]);
+  await knex("tasks").insert([{ id: 3, user_id: 1, name: "ジムに行く3" }]);
+  await knex("tasks").insert([{ id: 4, user_id: 1, name: "ジムに行く4" }]);
 }
